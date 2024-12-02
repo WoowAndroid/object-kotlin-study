316d5b7da18a9428620cdef025c0c62c6fc80955
somin <salth0601@gmail.com>
2024-12-02 19:21:36 +0900

    refactor: TicketSeller가 스스로 티켓을 판매하도록 수정
    - Theater의 enter내 if문을 ticketSeller의 sellTo로 이동
    - TicketOffice와 협력하는 TicketSeller의 내부 구현을 캡슐화

--- src/main/kotlin/s6m1n/chapter1/ticket/Theater.kt
+++ src/main/kotlin/s6m1n/chapter1/ticket/Theater.kt
@@ -2,14 +2,6 @@ package s6m1n.chapter1.ticket
 
 class Theater(private val ticketSeller: TicketSeller) {
     fun enter(audience: Audience) {
-        if (audience.bag.hasInvitation()) {
-            val ticket: Ticket = ticketSeller.ticketOffice.getTicket()
-            audience.bag.setTicket(ticket)
-        } else {
-            val ticket = ticketSeller.ticketOffice.getTicket()
-            audience.bag.minusAmount(ticket.fee)
-            ticketSeller.ticketOffice.plusAmount(ticket.fee)
-            audience.bag.setTicket(ticket)
-        }
+        ticketSeller.sellTo(audience)
     }
 }
--- src/main/kotlin/s6m1n/chapter1/ticket/TicketSeller.kt
+++ src/main/kotlin/s6m1n/chapter1/ticket/TicketSeller.kt
@@ -1,3 +1,15 @@
 package s6m1n.chapter1.ticket
 
-class TicketSeller(val ticketOffice: TicketOffice)
+class TicketSeller(private val ticketOffice: TicketOffice) {
+    fun sellTo(audience: Audience) {
+        if (audience.bag.hasInvitation()) {
+            val ticket: Ticket = ticketOffice.getTicket()
+            audience.bag.setTicket(ticket)
+        } else {
+            val ticket = ticketOffice.getTicket()
+            audience.bag.minusAmount(ticket.fee)
+            ticketOffice.plusAmount(ticket.fee)
+            audience.bag.setTicket(ticket)
+        }
+    }
+}
