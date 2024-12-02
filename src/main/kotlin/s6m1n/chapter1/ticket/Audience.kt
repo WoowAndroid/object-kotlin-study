8653ce2679054221bc64d042eb1e6e10354be38e
somin <salth0601@gmail.com>
2024-12-02 19:36:25 +0900

    refactor: Audience가 스스로 티켓을 구매하도록 수정
    - TicketSeller내 sellTo의 if문을 Audience의 buy로 이동
    - Bag과 협력하는 Audience의 내부 구현을 캡슐화

--- src/main/kotlin/s6m1n/chapter1/ticket/Audience.kt
+++ src/main/kotlin/s6m1n/chapter1/ticket/Audience.kt
@@ -1,3 +1,14 @@
 package s6m1n.chapter1.ticket
 
-class Audience(val bag: Bag)
+class Audience(private val bag: Bag) {
+    fun buy(ticket: Ticket): Long {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket)
+            return 0L
+        } else {
+            bag.minusAmount(ticket.fee)
+            bag.setTicket(ticket)
+            return ticket.fee
+        }
+    }
+}
--- src/main/kotlin/s6m1n/chapter1/ticket/TicketSeller.kt
+++ src/main/kotlin/s6m1n/chapter1/ticket/TicketSeller.kt
@@ -2,14 +2,8 @@ package s6m1n.chapter1.ticket
 
 class TicketSeller(private val ticketOffice: TicketOffice) {
     fun sellTo(audience: Audience) {
-        if (audience.bag.hasInvitation()) {
-            val ticket: Ticket = ticketOffice.getTicket()
-            audience.bag.setTicket(ticket)
-        } else {
-            val ticket = ticketOffice.getTicket()
-            audience.bag.minusAmount(ticket.fee)
-            ticketOffice.plusAmount(ticket.fee)
-            audience.bag.setTicket(ticket)
-        }
+        val ticket = ticketOffice.getTicket()
+        val fee = audience.buy(ticket)
+        ticketOffice.plusAmount(fee)
     }
 }
