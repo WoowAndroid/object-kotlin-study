413b33c2dec64af8fa4b44655a99244479340267
aprilgom <aprilgom@gmail.com>
2024-12-09 19:10:42 +0900

    refactor: 티켓 판매 메서드를 Theater에서 TicketSeller로 이동

--- src/main/kotlin/aprilgom/chapter1/Theater.kt
+++ src/main/kotlin/aprilgom/chapter1/Theater.kt
@@ -3,15 +3,9 @@ package aprilgom.chapter1
 class Theater(
     private val ticketSeller: TicketSeller,
 ) {
-    fun enter(audience: Audience) {
-        if(audience.bag.hasInvitation()){
-            val ticket = ticketSeller.ticketOffice.getTicket()
-            audience.bag.ticket = ticket
-        } else {
-            val ticket = ticketSeller.ticketOffice.getTicket()
-            audience.bag.minusAmount(ticket.fee)
-            ticketSeller.ticketOffice.plusAmount(ticket.fee)
-            audience.bag.ticket = ticket
-        }
-    }
+    // 이제 enter 메서드는 sellTo 메서드를 호출하는 간단한 코드로 바뀐다.
+    // 수정된 클래스 어디서도 ticketOffice에 접근하지 않는다는 사실에 주목.
+    // Theater는 단지 ticketSeller의 sellTo 메시지만 알고있다.
+
+    fun enter(audience: Audience) = ticketSeller.sellTo(audience)
 }
--- src/main/kotlin/aprilgom/chapter1/TicketSeller.kt
+++ src/main/kotlin/aprilgom/chapter1/TicketSeller.kt
@@ -1,5 +1,20 @@
 package aprilgom.chapter1
 
 class TicketSeller(
-    val ticketOffice: TicketOffice
-)
+    private val ticketOffice: TicketOffice
+    //ticketOffice가 private가 되었다는 사실에 주목하자.
+    //이제 TicketSeller는 판매 요금을 적립하는 일을 스스로 수행할 수 밖에 없다.
+    //이렇게 객체 내부의 세부적인 사항을 감추는 것을 캡슐화라고 부른다.
+) {
+    fun sellTo(audience: Audience) {
+        if(audience.bag.hasInvitation()){
+            val ticket = ticketOffice.getTicket()
+            audience.bag.ticket = ticket
+        } else {
+            val ticket = ticketOffice.getTicket()
+            audience.bag.minusAmount(ticket.fee)
+            ticketOffice.plusAmount(ticket.fee)
+            audience.bag.ticket = ticket
+        }
+    }
+}
