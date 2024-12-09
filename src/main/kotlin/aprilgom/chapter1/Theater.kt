e2a532b025f6392428513c2586256e3d20c782a9
aprilgom <aprilgom@gmail.com>
2024-12-09 19:26:09 +0900

    chore: 주석 삭제

--- src/main/kotlin/aprilgom/chapter1/Theater.kt
+++ src/main/kotlin/aprilgom/chapter1/Theater.kt
@@ -3,9 +3,5 @@ package aprilgom.chapter1
 class Theater(
     private val ticketSeller: TicketSeller,
 ) {
-    // 이제 enter 메서드는 sellTo 메서드를 호출하는 간단한 코드로 바뀐다.
-    // 수정된 클래스 어디서도 ticketOffice에 접근하지 않는다는 사실에 주목.
-    // Theater는 단지 ticketSeller의 sellTo 메시지만 알고있다.
-
     fun enter(audience: Audience) = ticketSeller.sellTo(audience)
 }
--- src/main/kotlin/aprilgom/chapter1/TicketSeller.kt
+++ src/main/kotlin/aprilgom/chapter1/TicketSeller.kt
@@ -2,9 +2,6 @@ package aprilgom.chapter1
 
 class TicketSeller(
     private val ticketOffice: TicketOffice
-    //ticketOffice가 private가 되었다는 사실에 주목하자.
-    //이제 TicketSeller는 판매 요금을 적립하는 일을 스스로 수행할 수 밖에 없다.
-    //이렇게 객체 내부의 세부적인 사항을 감추는 것을 캡슐화라고 부른다.
 ) {
     fun sellTo(audience: Audience) {
         if(audience.bag.hasInvitation()){
