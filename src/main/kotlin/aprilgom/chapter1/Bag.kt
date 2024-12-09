d92ad56f62ccf0a876f6b6f4e3c63fdfa6fc5496
aprilgom <aprilgom@gmail.com>
2024-12-09 18:40:18 +0900

    feat: Bag 구현

---
+++ src/main/kotlin/aprilgom/chapter1/Bag.kt
@@ -0,0 +1,24 @@
+package aprilgom.chapter1
+
+class Bag(
+    private var amount: Long = 0
+) {
+
+    constructor(
+        invitation: Invitation,
+        amount: Long
+    ): this(amount){
+        this.invitation = invitation
+    }
+
+    var ticket: Ticket? = null
+    var invitation: Invitation? = null
+    fun hasTicket() = ticket != null
+    fun plusAmount(amount: Long) {
+        this.amount += amount
+    }
+
+    fun minusAmount(amount: Long) {
+        this.amount -= amount
+    }
+}
