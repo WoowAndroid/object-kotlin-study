ee6c90c79aca8b708d73a42c4f3f9b583c3342fe
aprilgom <aprilgom@gmail.com>
2024-12-09 19:56:41 +0900

    chore: 주석 삭제

--- src/main/kotlin/aprilgom/chapter1/Audience.kt
+++ src/main/kotlin/aprilgom/chapter1/Audience.kt
@@ -2,8 +2,7 @@ package aprilgom.chapter1
 
 class Audience(
     private val bag: Bag
-){
-    //자신의 소지품을 스스로 관리한다.
+) {
     fun buy(ticket: Ticket): Long {
         if(bag.hasInvitation()) {
             bag.ticket = ticket
--- src/main/kotlin/aprilgom/chapter1/TicketSeller.kt
+++ src/main/kotlin/aprilgom/chapter1/TicketSeller.kt
@@ -4,7 +4,6 @@ class TicketSeller(
     private val ticketOffice: TicketOffice
 ) {
     fun sellTo(audience: Audience) {
-        //자신의 소지품을 스스로 관리한다.
         ticketOffice.plusAmount(
             audience.buy(ticketOffice.getTicket())
         )
