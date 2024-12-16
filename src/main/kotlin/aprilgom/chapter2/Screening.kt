f0775e0f27bf45f9da8d8ef913861cf7594a6d93
aprilgom <aprilgom@gmail.com>
2024-12-16 21:14:47 +0900

    feat(Screening): reserve와 calculateFee 구현

--- src/main/kotlin/aprilgom/chapter2/Screening.kt
+++ src/main/kotlin/aprilgom/chapter2/Screening.kt
@@ -9,4 +9,10 @@ class Screening(
 ) {
     fun isSequence(sequence: Int): Boolean = this.sequence == sequence
     fun getMovieFee() = movie.getFee()
+
+    fun reserve(customer: Customer, audienceCount: Int): Reservation =
+        Reservation(customer, this, calculateFee(audienceCount), audienceCount)
+
+    private fun calculateFee(audienceCount: Int): Money =
+        movie.calculateMovieFee(this).times(audienceCount)
 }
